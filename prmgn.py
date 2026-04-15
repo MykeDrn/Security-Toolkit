05503f610e637fb2d18c4fb8642a3861cef1e954
Magus <54345778+MykeDrn@users.noreply.github.com>
2026-04-14 21:16:41 -0300

    Refactor to class-based primitive generator
    Refactor primitive generator into a class with methods for password, token, and URL-safe token generation.

--- prmgn.py
+++ prmgn.py
@@ -4,20 +4,33 @@
 import string
 import secrets 
 
-caracteres = string.ascii_letters + string.digits + string.punctuation
+class generator:
+    def __init__(self, tamanho=64):
+        self.caracteres = string.ascii_letters + string.digits + string.punctuation # Define os caracteres permitidos para a senha
+        self.tamanho=tamanho # Define o tamanho da senha ou token a ser gerado
+    
+    def password(self):
+        while True:
+            primitiva = ''.join(secrets.choice(self.caracteres) for _ in range(self.tamanho))
+            # Validação técnica
+            if (any(c.islower() for c in primitiva) 
+                and any(c.isupper() for c in primitiva) 
+                and any(c.isdigit() for c in primitiva) 
+                and any(c in string.punctuation for c in primitiva)):
+                return primitiva
+    
+    def token(self):
+        return secrets.token_hex(self.tamanho // 2)  # Gera um token hexadecimal
 
-def prim_gen():
-    while True:
-        primitiva = ''.join(secrets.choice(caracteres) for _ in range(64)) # gera uma primitiva aleatória de 64 caracteres.
-        if (any(c.islower() for c in primitiva) 
-            and any(c.isupper() for c in primitiva) 
-            and any(c.isdigit() for c in primitiva) 
-            and any(c in string.punctuation for c in primitiva)):
-            return primitiva # verifica se a primitiva tem ao menos um caractere de cada tipo.
-        
+    def token_urlsafe(self):
+        return secrets.token_urlsafe(self.tamanho)  # Gera um token seguro para URLs
+ 
     
 # teste de uso
-primitiva_gerada = prim_gen()
-if primitiva_gerada:
-    print("Primitiva gerada:", primitiva_gerada)
-    
+
+senha = generator(tamanho=16).password()
+print(f'Primitiva de senha: {senha}')
+token = generator(tamanho=32).token()
+print(f'Primitiva de token: {token}')
+token_urlsafe = generator(tamanho=32).token_urlsafe()
+print(f'Primitiva de token URL seguro: {token_urlsafe}')
