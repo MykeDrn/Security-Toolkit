392a6f1e1a76499563250dc794c3cd14c78a077e
Magus <54345778+MykeDrn@users.noreply.github.com>
2026-04-20 22:41:59 -0300

    Enhance generator with entropy and evaluation features
    Added entropy calculation and evaluation methods to the generator class.

--- prmgn.py
+++ prmgn.py
@@ -1,10 +1,12 @@
-'''Gerador de primitivas'''
+"""Gerador de primitivas"""
 '''Este algoritmo usa o módulo 'secrets' para gerar primitivas seguras.'''
 
 import string
-import secrets 
+import secrets
+import math
+from collections import Counter
 
-class generator:
+class Generator:
     def __init__(self, tamanho=64):
         self.caracteres = string.ascii_letters + string.digits + string.punctuation # Define os caracteres permitidos para a senha
         self.tamanho=tamanho # Define o tamanho da senha ou token a ser gerado
@@ -24,13 +26,36 @@ class generator:
 
     def token_urlsafe(self):
         return secrets.token_urlsafe(self.tamanho)  # Gera um token seguro para URLs
- 
-    
-# teste de uso
 
-senha = generator(tamanho=16).password()
+    def entropia(self, data):
+        if not data:
+            return 0
+
+        counts = Counter(data)
+        total_len = len(data)
+        entropia = 0
+
+        for count in counts.values():
+            p = count / total_len
+            entropy -= p * math.log2(p)
+        return entropia
+
+    def avaliar(self, primitiva):
+        entropia = self.entropia(primitiva)
+        score = (entropia / 6.55) * 100
+
+        if score > 70:
+            return f"Nível de Confiança: ALTO ({score:.1f}%)"
+        else:
+            return f"Nível de Confiança: MÉDIO ({score:.1f}%)"
+
+# teste de uso
+gen = Generator(tamanho=64)
+senha = gen.password()
 print(f'Primitiva de senha: {senha}')
-token = generator(tamanho=32).token()
+token = gen.token()
 print(f'Primitiva de token: {token}')
-token_urlsafe = generator(tamanho=32).token_urlsafe()
+token_urlsafe =gen.token_urlsafe()
 print(f'Primitiva de token URL seguro: {token_urlsafe}')
+teste = gen.avaliar(senha)
+print(teste)
